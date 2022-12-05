f745fe11ca91d5e1fe50ef1c156644ab2b516cdd
henok keraga <henokkeraga@gmail.com>
2022-12-04 17:34:07 -0700

    sf

--- simple-js/npm_intro/index.js
+++ simple-js/npm_intro/index.js
@@ -1,2 +1,19 @@
-var chalk = require("chalk");
-console.log(chalk.blue('hello'))
+var url = require("url");
+var util = require("util");
+var os = require("os");
+var date=require('./date.js')
+
+var urlAddress = "http://localhost:8080/user?year=2022";
+
+var parse_url = url.parse(urlAddress, true);
+
+console.log(parse_url);
+
+console.log(parse_url.host);
+console.log(util.format(" %s hello", "henok"));
+
+console.log(os.arch());
+console.log(os.platform());
+console.log(os.cpus().length);
+console.log(os.hostname());
+console.log(date())
